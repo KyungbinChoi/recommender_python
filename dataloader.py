114a667d03b8f90da6a338379f8f1c46a6c2a532
KyungbinChoi <iris122413@gmail.com>
2022-09-08 15:57:48 +0900

    jobs 20220908

--- dataloader.py
+++ dataloader.py
@@ -8,16 +8,39 @@ import numpy as np
 
 class movielens_dataloader:
     def __init__(self) -> None:
+        """
+        setting constant terms & data path
+        """
+        os.chdir('./movielens/data')
+        self.rating_path = "./ratings.csv"
+        self.item_path = "./movies.csv"
+        self.tags_path = "./tags.csv"
+        
+        self.rating_cols = []
+        self.items_cols = []
+        self.tags_cols = []
         pass
 
     def get_rating_data(self):
-        
+        """
+            load rating dataframe
+        """
         return
     
     def get_item_data(self):
-        
+        """
+            load item (movies) dataframe
+        """
+        return
+
+    def get_tags_data(self):
+        """
+            load user-item tagging dataframe
+        """
         return
 
     def get_pivoting_dataframe(self):
-        
+        """
+            construct user-item matrix 
+        """
         return
