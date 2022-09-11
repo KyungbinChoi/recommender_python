caba67b0947f5f59a049744b95c94597157dc5f2
KyungbinChoi <iris122413@gmail.com>
2022-09-11 15:22:00 +0900

    dataloader & contents based method module

--- dataloader.py
+++ dataloader.py
@@ -16,27 +16,48 @@ class movielens_dataloader:
         self.item_path = "./movies.csv"
         self.tags_path = "./tags.csv"
         
-        self.rating_cols = []
-        self.items_cols = []
-        self.tags_cols = []
+        self.rating_cols = ["userId", 'movieId', 'rating', 'timestamp']
+        self.items_cols = ["movieId", 'title', 'genres']
+        self.tags_cols = ["userId", 'movieId', 'tags', 'timestamp']
         pass
 
     def get_rating_data(self):
         """
             load rating dataframe
         """
-        return
+        ratings = pd.read_csv(self.rating_path)
+        for col in self.ratiing_cols:
+            if col in ratings.columns:
+                pass
+            else:
+                print("Not contatined column or different name. Column name : {}".format(col))
+        print("shape of data : {}".format(ratings.shape))
+        return ratings
     
     def get_item_data(self):
         """
             load item (movies) dataframe
         """
-        return
+        items = pd.read_csv(self.item_path)
+        for col in self.items_cols:
+            if col in items.columns:
+                pass
+            else:
+                print("Not contatined column or different name. Column name : {}".format(col))
+        print("shape of data : {}".format(items.shape))
+        return items
 
     def get_tags_data(self):
         """
             load user-item tagging dataframe
         """
+        tag = pd.read_csv(self.tags_path)
+        for col in self.tags_cols:
+            if col in tag.columns:
+                pass
+            else:
+                print("Not contatined column or different name. Column name : {}".format(col))
+        print("shape of data : {}".format(tag.shape))
         return
 
     def get_pivoting_dataframe(self):
--- movielens/Contents_based.py
+++ movielens/Contents_based.py
@@ -1,3 +1,16 @@
 """
 Contents based recommender
 """
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def pre_process():
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
